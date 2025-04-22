3feef5c0d8a97ecdb5f218dd54814d1e010f2e78
Pratik <pratikmali8130@gmail.com>
2025-04-22 12:03:11 +0530

    Add this

---
+++ Cond_Control_statement.py
@@ -0,0 +1,18 @@
+# Write a program to check whether given number is even 
+'''
+n = int(input("Enter the number:"))
+
+if n % 2 == 0:
+    print(f"The given number {n} is even")
+else:
+    print(f"The given number {n} is odd")
+'''
+
+# Write a program to check whether given number is positive or negative
+
+n = int(input("Enter the number:"))
+
+if n > 0 :
+    print(f"The given number {n} is positive")
+else:
+    print(f"The given number {n} is negative")
