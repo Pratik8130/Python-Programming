df72dbc59add750e06dfdb878646d52eed483068
Pratik <pratikmali8130@gmail.com>
2025-04-23 13:16:55 +0530

    Looping statements

--- Cond_Control_statement.py
+++ Cond_Control_statement.py
@@ -9,10 +9,23 @@ else:
 '''
 
 # Write a program to check whether given number is positive or negative
-
+'''
 n = int(input("Enter the number:"))
 
 if n > 0 :
     print(f"The given number {n} is positive")
 else:
     print(f"The given number {n} is negative")
+'''
+# Ticket price based on age
+#1.Write a program to display ticket price based on age 
+# Task - set ticket prices as follows
+# 1. Age > 5 = free (no cost)
+# 2. 5 <= Age <= 18 = 100 Rs
+# 3. 19 <= Age <=60 = 200 Rs
+# 4. Age > 60 = 150 Rs 
+# Take age as the input and print the price
+
+# 2. Login Authentication 
+# Task - set a correct username and password ask the user to input both and check these values using if else statements you have to check whether entered credentials ot valid or not
+
---
+++ Looping_statements.py
@@ -0,0 +1,30 @@
+# Program to display list of even numbers present in the range of 1 to 20
+'''
+ for i in range(2,21,2):
+   print(i)
+'''
+
+# Program to display sum of numbers from 1 to 100 using for loop
+'''
+sum = 0
+for i in range(101):
+    sum = sum + i
+print(sum)
+'''
+# Loop through a list of fruits and print each fruit value 
+'''
+Fruits = ["Apple","Banana","Cherry","Mango"]
+for fruit in Fruits:
+    print(fruit)
+'''
+# Loop through a list of numbers and print only the numbers greater than 50
+'''
+Number = [12,75,150,180,145,525,50]
+for num in Number:
+    if num > 50:
+        print(num)
+'''
+
+n = 5
+for i in range(1,6):
+    print("*" * i)
